4b20cf59a0ec8fe35cf0e49c4dbb91cab2f6798b
Alexander Sinyukhin <asinyukhin@inbox.ru>
2022-10-16 12:47:56 +0200

    Initial commit

---
+++ .gdbinit
@@ -0,0 +1,5 @@
+define target hookpost-remote
+file "stm32_template.elf"
+monitor reset
+break main
+end
---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdint.h>
+#include <stm32f10x.h>
+
+void delay(uint32_t ticks) {
+	for (int i=0; i<ticks; i++) {
+		__NOP();
+	}
+}
+
+int __attribute((noreturn)) main(void) {
+	// Enable clock for AFIO
+	RCC->APB2ENR |= RCC_APB2ENR_AFIOEN;
+	// Enable clock for GPIOC
+	RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
+	// Enable PC13 push-pull mode
+	GPIOC->CRH &= ~GPIO_CRH_CNF13; //clear cnf bits
+	GPIOC->CRH |= GPIO_CRH_MODE13_0; //Max speed = 10Mhz
+
+    while (1) {
+	    GPIOC->ODR |= (1U<<13U); //U -- unsigned suffix (to avoid syntax warnings in IDE)
+		delay(1000000);
+	    GPIOC->ODR &= ~(1U<<13U);
+	    delay(1000000);
+    }
+}
