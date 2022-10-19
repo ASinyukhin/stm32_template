0e6236725fe6a415af0ce9f032738411468a93d5
Alexander Sinyukhin <asinyukhin@inbox.ru>
2022-10-19 16:17:19 +0200

    Timer example (not working state)

--- main.c
+++ main.c
@@ -2,36 +2,50 @@
 //#include <stm32f10x.h> //Not used when we use libopencm3
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/timer.h>
+#include <libopencmsis/core_cm3.h>
+
+#include <system_stm32f10x.h>
 
 void delay(uint32_t ticks) {
 	for (int i=0; i<ticks; i++) {
-		//__NOP();
 		__asm volatile("nop");
 	}
 }
 
-int __attribute((noreturn)) main(void) {
-	#if 0
-	// Enable clock for AFIO
-	RCC->APB2ENR |= RCC_APB2ENR_AFIOEN;
-	// Enable clock for GPIOC
-	RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
-	// Enable PC13 push-pull mode
-	GPIOC->CRH &= ~GPIO_CRH_CNF13; //clear cnf bits
-	GPIOC->CRH |= GPIO_CRH_MODE13_0; //Max speed = 10Mhz
+void tim2_isr(void) {
+	gpio_toggle(GPIOC, GPIO13);
+	timer_clear_flag(TIM2, TIM_SR_UIF);
+}
 
-    while (1) {
-	    GPIOC->ODR |= (1U<<13U); //U -- unsigned suffix (to avoid syntax warnings in IDE)
-		delay(1000000);
-	    GPIOC->ODR &= ~(1U<<13U);
-	    delay(1000000);
-    }
-	#endif
-	RCC_APB2ENR |= RCC_APB2ENR_IOPCEN; //TODO: check for rcc_ function to enable gpio clock
+int __attribute((noreturn)) main(void) {
+	//RCC_APB2ENR |= RCC_APB2ENR_IOPCEN; //TODO: check for rcc_ function to enable gpio clock
+	rcc_periph_clock_enable(RCC_GPIOC);
+	rcc_periph_clock_enable(RCC_USART2);
 
 	gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
+
+	/* Timer example */
+	uint32_t tim_pre = 1024;
+	rcc_periph_clock_enable(RCC_TIM2);
+	rcc_periph_reset_pulse(RST_TIM2);
+
+	nvic_enable_irq(NVIC_TIM2_IRQ);
+
+	timer_set_mode(TIM2, TIM_CR1_CKD_CK_INT, TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
+	timer_set_prescaler(TIM2, tim_pre);
+	timer_disable_preload(TIM2);
+	timer_set_period(TIM2, 30000);
+	timer_enable_irq(TIM2, TIM_DIER_UIE); //update IRQ
+	
+	//nvic_enable_irq(NVIC_TIM2_IRQ);
+	nvic_clear_pending_irq(NVIC_TIM2_IRQ);
+
+	timer_enable_counter(TIM2);
+
+	gpio_set(GPIOC, GPIO13);
 	while (1) {
-		gpio_toggle(GPIOC, GPIO13);
+		//gpio_toggle(GPIOC, GPIO13);
 		delay(1000000);
 	}
 }
