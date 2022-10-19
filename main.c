b0efbf09821f94722c49353eb5a698c2fa617738
Alexander Sinyukhin <asinyukhin@inbox.ru>
2022-10-19 14:09:48 +0200

    Working libopencm3 gpio example

--- main.c
+++ main.c
@@ -1,13 +1,17 @@
 #include <stdint.h>
-#include <stm32f10x.h>
+//#include <stm32f10x.h> //Not used when we use libopencm3
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/rcc.h>
 
 void delay(uint32_t ticks) {
 	for (int i=0; i<ticks; i++) {
-		__NOP();
+		//__NOP();
+		__asm volatile("nop");
 	}
 }
 
 int __attribute((noreturn)) main(void) {
+	#if 0
 	// Enable clock for AFIO
 	RCC->APB2ENR |= RCC_APB2ENR_AFIOEN;
 	// Enable clock for GPIOC
@@ -22,4 +26,12 @@ int __attribute((noreturn)) main(void) {
 	    GPIOC->ODR &= ~(1U<<13U);
 	    delay(1000000);
     }
+	#endif
+	RCC_APB2ENR |= RCC_APB2ENR_IOPCEN; //TODO: check for rcc_ function to enable gpio clock
+
+	gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
+	while (1) {
+		gpio_toggle(GPIOC, GPIO13);
+		delay(1000000);
+	}
 }
