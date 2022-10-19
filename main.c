305a8fe7cc66b29749309f3165aa59a66e226c98
Alexander Sinyukhin <asinyukhin@inbox.ru>
2022-10-19 17:49:27 +0200

    Quick fix interrupt problem

--- main.c
+++ main.c
@@ -13,7 +13,17 @@ void delay(uint32_t ticks) {
 	}
 }
 
-void tim2_isr(void) {
+void HardFault_Handler(void) {
+	while (1) {
+		;
+	}
+}
+
+//If we use CMSIC interrupts table, we do not want have them redefined in libopencmsis
+//Otherwise we should use only libopencm3 irq table and it's startup
+#undef TIM2_IRQHandler
+//This is a workaround and should be fixed in future
+void TIM2_IRQHandler(void) {
 	gpio_toggle(GPIOC, GPIO13);
 	timer_clear_flag(TIM2, TIM_SR_UIF);
 }
@@ -25,12 +35,10 @@ int __attribute((noreturn)) main(void) {
 
 	gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
 
-	/* Timer example */
+	/* Timer blinking example */
 	uint32_t tim_pre = 1024;
 	rcc_periph_clock_enable(RCC_TIM2);
 	rcc_periph_reset_pulse(RST_TIM2);
-
-	nvic_enable_irq(NVIC_TIM2_IRQ);
 
 	timer_set_mode(TIM2, TIM_CR1_CKD_CK_INT, TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
 	timer_set_prescaler(TIM2, tim_pre);
@@ -38,14 +46,11 @@ int __attribute((noreturn)) main(void) {
 	timer_set_period(TIM2, 30000);
 	timer_enable_irq(TIM2, TIM_DIER_UIE); //update IRQ
 	
-	//nvic_enable_irq(NVIC_TIM2_IRQ);
+	nvic_enable_irq(NVIC_TIM2_IRQ);
 	nvic_clear_pending_irq(NVIC_TIM2_IRQ);
-
 	timer_enable_counter(TIM2);
 
-	gpio_set(GPIOC, GPIO13);
 	while (1) {
-		//gpio_toggle(GPIOC, GPIO13);
-		delay(1000000);
+		__asm volatile ("nop");
 	}
 }
